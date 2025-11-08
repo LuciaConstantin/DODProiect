035160615f8f49c589d0be42081b35b5344bc263
LuciaConstantin <luciamihaela.constantin26@gmail.com>
2025-11-08 15:52:48 +0200

    From oop to dod.

---
+++ DODProiect/dodmain.cpp
@@ -0,0 +1,380 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_timer.h>
+#include <SDL3/SDL_stdinc.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
+namespace Colors {
+	const SDL_Color BLACK = { 0, 0, 0, SDL_ALPHA_OPAQUE };
+	const SDL_Color GREEN = { 0, 255, 0, SDL_ALPHA_OPAQUE };
+	const SDL_Color WHITE = { 255, 255, 255, SDL_ALPHA_OPAQUE };
+	const SDL_Color MINT = { 143, 206, 150, SDL_ALPHA_OPAQUE };
+}
+
+namespace Graphics {
+	struct Screen {
+		const int width = 1800;
+		const int height = 900;
+		const int centerX = width / 2;
+		const int centerY = height / 2;
+		const int messageSpace = 50;
+	};
+
+	struct entityDimensions {
+		const int w = 20;
+		const int h = 20;
+		const float step = 0.5;
+	};
+
+}
+
+
+struct App {
+
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+
+	Graphics::Screen screen;
+	Graphics::entityDimensions dim;
+
+	Uint64 totalFrameTicks = 0;
+	Uint64 totalFrames = 0;
+
+} app;
+
+
+bool InitSDL() {
+	if (!SDL_Init(SDL_INIT_VIDEO)) {
+		std::cout << "SDL_Init failed with error: " << SDL_GetError() << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+
+static void ClearScreen(SDL_Renderer* renderer) {
+	SDL_SetRenderDrawColor(renderer, Colors::BLACK.r, Colors::BLACK.g, Colors::BLACK.b, Colors::BLACK.a);
+	SDL_RenderClear(renderer);
+}
+
+
+void ShutdownApplication() {
+	if (app.window != nullptr) {
+		SDL_DestroyWindow(app.window);
+		app.window = nullptr;
+	}
+
+	if (app.renderer != nullptr) {
+		SDL_DestroyRenderer(app.renderer);
+		app.renderer = nullptr;
+	}
+
+	SDL_Quit();
+}
+
+bool InitApplication() {
+
+	if (!InitSDL()) {
+		ShutdownApplication();
+		return false;
+	}
+
+	app.window = SDL_CreateWindow("Hello window", app.screen.width, app.screen.height, SDL_WINDOW_OPENGL);
+
+	if (!app.window) {
+		std::cout << "Unable to create the main window. Error: " << SDL_GetError() << std::endl;
+		ShutdownApplication();
+		return false;
+	}
+
+	app.renderer = SDL_CreateRenderer(app.window, nullptr);
+
+
+	return true;
+}
+
+
+void create(int N, float*& x, float*& y, int*& w, int*& h, float*& step, float*& xRand, float*& yRand) {
+	srand((unsigned)time(NULL));
+
+	for (int i = 0; i < N; i++) {
+		w[i] = app.dim.w;
+		h[i] = app.dim.h;
+		step[i] = app.dim.step;
+
+	}
+
+	for (int i = 0; i < N; i++) {
+		x[i] = (float)(rand() % (app.screen.width - w[i]));
+		y[i] = (float)(app.screen.messageSpace + rand() % (app.screen.height - app.screen.messageSpace - h[i]));
+
+		int minVal = -2;
+		int maxVal = 2;
+
+		do {
+			xRand[i] = (float)(rand() % (maxVal - minVal + 1) + minVal);
+			yRand[i] = (float)(rand() % (maxVal - minVal + 1) + minVal);
+		} while (xRand[i] == 0 && yRand[i] == 0);
+
+	}
+
+}
+
+void drawEntities(int N, float* x, float* y, int* w, int* h, SDL_Renderer* renderer) {
+
+	for (int i = 0; i < N; i++) {
+		SDL_FRect rect;
+		rect.x = (float)x[i];
+		rect.y = (float)y[i];
+		rect.h = (float)h[i];
+		rect.w = (float)w[i];
+
+		SDL_SetRenderDrawColor(renderer, Colors::MINT.r, Colors::MINT.g, Colors::MINT.b, Colors::MINT.a);
+		SDL_RenderFillRect(renderer, &rect);
+
+	}
+
+}
+
+
+void moveEntity(int N, float*& x, float*& y, int* w, int* h, float* step, float* xRand, float* yRand) {
+
+	for (int i = 0; i < N; i++) {
+		x[i] = x[i] + xRand[i] * step[i];
+		y[i] = y[i] + yRand[i] * step[i];
+	}
+}
+
+
+void checkOutOfFrame(int N, float*& x, float*& y, int* w, int* h, float* step, float*& xRand, float*& yRand) {
+	for (int i = 0; i < N; i++) {
+		float futureX = x[i] + xRand[i] * step[i];
+		float futureY = y[i] + yRand[i] * step[i];
+
+		if (futureX < 0) {
+			x[i] = 0.f;
+			xRand[i] = -xRand[i];
+		}
+		else if (futureX + w[i] >= app.screen.width) {
+			x[i] = (float)app.screen.width - w[i];
+			xRand[i] = -xRand[i];
+		}
+
+		if (futureY < app.screen.messageSpace) {
+			y[i] = (float)app.screen.messageSpace;
+			yRand[i] = -yRand[i];
+
+		}
+		else if (futureY + h[i] >= app.screen.height) {
+			y[i] = (float)app.screen.height - h[i];
+			yRand[i] = -yRand[i];
+		}
+
+	}
+}
+
+
+float checkOutOfScreenX(float x, int w) {
+	if (x + w > app.screen.width) {
+		x = (float)app.screen.width - w;
+	}
+
+	else if (x < 0) {
+		x = 0.f;
+	}
+
+	return x;
+}
+
+float checkOutOfScreenY(float y, int h) {
+	if (y + h > app.screen.height) {
+		y = (float)app.screen.height - h;
+	}
+	else if (y < app.screen.messageSpace) {
+		y = (float)app.screen.messageSpace;
+	}
+	return y;
+}
+
+
+
+bool checkCollision(float x1, float y1, float x2, float y2, int w1, int h1, int w2, int h2) {
+	return (x1 < x2 + w2) && (x1 + w1 > x2) && (y1 < y2 + h2) && (y1 + h1 > y2);
+}
+
+void collision(int N, float*& x, float*& y, float* step, float*& xRand, float*& yRand, int* w, int* h) {
+
+	for (int i = 0; i < N; i++) {
+		for (int j = i + 1; j < N; j++) {
+			if (checkCollision(x[i], y[i], x[j], y[j], w[i], h[i], w[j], h[j])) {
+
+				float overlapX = 0.f;
+				float overlapY = 0.f;
+
+				if (x[i] < x[j]) {
+					overlapX = x[i] + w[i] - x[j];
+				}
+				else {
+					overlapX = x[j] + w[j] - x[i];
+				}
+
+				if (x[i] < x[j]) {
+					overlapY = y[i] + h[i] - y[j];
+				}
+				else {
+					overlapY = y[j] + h[j] - y[i];
+
+				}
+
+				if (overlapX < overlapY) {
+					xRand[i] = -xRand[i];
+					xRand[j] = -xRand[j];
+
+					if (x[i] < x[j]) {
+						x[i] = checkOutOfScreenX(x[i] - overlapX / 2, w[i]);
+						x[j] = checkOutOfScreenX(x[j] + overlapX / 2, w[j]);
+					}
+					else {
+						x[i] = checkOutOfScreenX(x[i] + overlapX / 2, w[i]);
+						x[j] = checkOutOfScreenX(x[j] - overlapX / 2, w[j]);
+					}
+				}
+
+				else {
+					yRand[i] = -yRand[i];
+					yRand[j] = -yRand[j];
+
+					if (y[i] < y[j]) {
+						y[i] = checkOutOfScreenY(y[i] - overlapY / 2, h[i]);
+						y[j] = checkOutOfScreenY(y[j] + overlapY / 2, h[j]);
+					}
+					else {
+						y[i] = checkOutOfScreenY(y[i] + overlapY / 2, h[i]);
+						y[j] = checkOutOfScreenY(y[j] - overlapY / 2, h[j]);
+					}
+				}
+			}
+
+		}
+	}
+}
+
+
+
+int main(int argc, char* argv[]) {
+
+	int nEntity = 1500; // number of entities
+
+	float* x = new float[nEntity];
+	float* y = new float[nEntity];
+	float* step = new float[nEntity];
+	float* xRand = new float[nEntity];
+	float* yRand = new float[nEntity];
+	int* w = new int[nEntity];
+	int* h = new int[nEntity];
+
+	create(nEntity, x, y, w, h, step, xRand, yRand);
+
+
+
+	if (!InitApplication()) {
+		ShutdownApplication();
+		return EXIT_FAILURE;
+	}
+
+	SDL_Event event;
+	bool running = true;
+
+	Uint64 perCountFrequency = SDL_GetPerformanceFrequency();
+	Uint64 lastCounter = SDL_GetPerformanceCounter();
+
+	double totalTimeMs = 0;
+	unsigned int frameCount = 0;
+
+	while (running) {
+		ClearScreen(app.renderer);
+
+
+		checkOutOfFrame(nEntity, x, y, w, h, step, xRand, yRand);
+		moveEntity(nEntity, x, y, w, h, step, xRand, yRand);
+		collision(nEntity, x, y, step, xRand, yRand, w, h);
+
+
+		// keyboard events
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_EVENT_KEY_DOWN:
+				if (event.key.scancode == SDL_SCANCODE_ESCAPE)
+					running = false;
+				break;
+
+			case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
+				running = false;
+				break;
+
+			case SDL_EVENT_QUIT:
+				running = false;
+				break;
+
+			default:
+				break;
+			}
+		}
+
+		drawEntities(nEntity, x, y, w, h, app.renderer);
+
+
+		// fps information
+		Uint64 endCounter = SDL_GetPerformanceCounter();
+		Uint64 counterElapsed = endCounter - lastCounter;
+
+		double MSPerFrame = (((1000.0f * (double)counterElapsed) / (double)perCountFrequency));
+		double FPS = (double)perCountFrequency / (double)counterElapsed;
+
+		lastCounter = endCounter;
+
+		totalTimeMs += MSPerFrame;
+		frameCount++;
+
+		double avgFPS = 1000.0 * (double)frameCount / (double)totalTimeMs;
+
+		char* fps = NULL;
+		char* avg = NULL;
+		char* ms = NULL;
+
+		SDL_asprintf(&fps, "Current FPS: %.2f", FPS);
+		SDL_asprintf(&avg, "Average FPS: %.2f", avgFPS);
+		SDL_asprintf(&ms, "Frame Time(ms): %.2f", MSPerFrame);
+
+
+
+		SDL_SetRenderDrawColor(app.renderer, 255, 255, 255, 255);
+		SDL_RenderDebugText(app.renderer, 10, 10, fps);
+		SDL_RenderDebugText(app.renderer, 10, 20, avg);
+		SDL_RenderDebugText(app.renderer, 10, 30, ms);
+
+
+		SDL_RenderPresent(app.renderer);
+
+
+	}
+
+
+	delete[] x;
+	delete[] y;
+	delete[] w;
+	delete[] h;
+	delete[] xRand;
+	delete[] yRand;
+	delete[] step;
+
+
+	ShutdownApplication();
+
+	return EXIT_SUCCESS;
+
+}
